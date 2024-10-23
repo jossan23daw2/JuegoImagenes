d8eaa9db17711b68b0a85c5c034541f98902a4b9
jossan23daw2 <15586411.clot@fje.edu>
2024-10-23 19:55:26 +0200

    Main Menu

--- app/src/main/java/com/example/juegominecraft/MainActivity.java
+++ app/src/main/java/com/example/juegominecraft/MainActivity.java
@@ -2,23 +2,48 @@ package com.example.juegominecraft;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.appcompat.widget.Toolbar;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CoordinatorLayout coordinatorLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        coordinatorLayout = (CoordinatorLayout) findViewById(R.id.coordinator);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()==R.id.action_ayuda){
+            ayuda();
+            return true;
+        }
+        return false;
+
+    }
+
+    private void ayuda() {
+        Snackbar.make(coordinatorLayout, "Ayuda", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
     }
 }
